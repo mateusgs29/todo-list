4ad6332d2b7bcd287a887720a9893feb7d91cb15
Mateus Gomes <mateus.gs1999@gmail.com>
2024-01-23 10:47:33 -0300

    projeto inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { AddTask } from './components/AddTask'
+import { Header } from './components/Header'
+import { ListTasks } from './components/ListTasks'
+
+import styles from './App.module.css'
+import './global.css'
 
 function App() {
+  // const tasks = [
+  //   {
+  //     id: 1,
+  //     title: 'Estudar programação',
+  //     completed: false
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Ler livros',
+  //     completed: true
+  //   }
+  // ]
 
   return (
-    <div />
+    <>
+      <Header /> 
+
+      <main className={styles.container}>
+        <AddTask /> 
+
+        <ListTasks />
+      </main>
+    </>
   )
 }
 
