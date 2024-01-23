b8c95d0740492d077c38d8afd8c5632937655991
Mateus Gomes <mateus.gs1999@gmail.com>
2024-01-23 14:50:33 -0300

    adicionado logica de adicionar tarefa

--- src/components/AddTask/index.tsx
+++ src/components/AddTask/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { PlusCircle } from "@phosphor-icons/react";
 import styles from './AddTask.module.css'
 
-export function AddTask() {
+interface AddTaskProps {
+    onAddNewTask: (task: string) => void
+}
+
+export function AddTask({ onAddNewTask }: AddTaskProps) {
+    const [newTask, setNewTask] = useState('')
+    
     return (
-        <form className={styles.addTask}>
+        <form 
+            className={styles.addTask} 
+            onSubmit={(e) => {
+                e.preventDefault()
+                onAddNewTask(newTask)
+                setNewTask('')
+            }}
+        >
             <input 
                 type="text"
                 placeholder="Adicione uma nova tarefa"
+                value={newTask}
+                onChange={e => setNewTask(e.target.value)}
             />
-            <button type="submit">
+            <button 
+                type="submit"
+                disabled={newTask.length === 0}
+            >
                 Criar
                 <PlusCircle size={16} />
             </button>
