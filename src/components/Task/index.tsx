81ac38fc89306af7f3469a9fc63d1e3887a3bc31
Mateus Gomes <mateus.gs1999@gmail.com>
2024-01-23 14:51:18 -0300

    ajustes finais

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -3,16 +3,38 @@ import { Trash } from "@phosphor-icons/react";
 import styles from './Task.module.css'
 import { useState } from "react";
 
-export function Task() {
-    // const [test, setTest] = useState(false)
+interface TaskProps {
+    id: string
+    description: string
+    completed: boolean
+    onCompleteTask: (id: string) => void
+    onDeleteTask: (id: string) => void
+}
+
+export function Task({ 
+    id, 
+    description, 
+    completed,
+    onCompleteTask,
+    onDeleteTask 
+}: TaskProps) {
+    const [check, setCheck] = useState(completed)
 
     return (
         <div className={styles.task}>
-            <input className={styles.checkbox} type="checkbox" />
-            <p>
-            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+            <input 
+                className={styles.checkbox} 
+                type="checkbox" 
+                checked={check}
+                onChange={() => {
+                    setCheck(!check)
+                    onCompleteTask(id)
+                }}
+            />
+            <p className={check ? styles.taskCheck : ''}>
+                {description}
             </p>
-            <button>
+            <button onClick={() => onDeleteTask(id)}>
                 <Trash size={20} />
             </button>
         </div>
