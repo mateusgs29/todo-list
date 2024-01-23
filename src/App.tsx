83177b1d0393ed449641fe97a39d9f4b6a8dbafe
Mateus Gomes <mateus.gs1999@gmail.com>
2024-01-23 14:52:53 -0300

    funcao de gerar id e de adicionar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { AddTask } from './components/AddTask'
 import { Header } from './components/Header'
 import { ListTasks } from './components/ListTasks'
@@ -5,28 +7,40 @@ import { ListTasks } from './components/ListTasks'
 import styles from './App.module.css'
 import './global.css'
 
+interface ITask {
+  id: string
+  description: string
+  completed: boolean
+}
+
 function App() {
-  // const tasks = [
-  //   {
-  //     id: 1,
-  //     title: 'Estudar programação',
-  //     completed: false
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Ler livros',
-  //     completed: true
-  //   }
-  // ]
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  const generateId = () => {
+    const timestamp = new Date().getTime().toString(16)
+    const random = Math.random().toString(16).slice(2)
+
+    return `${timestamp}${random}`
+  }
+
+  const addNewTask = (task: string) => {
+    const newTask = {
+      id: generateId(),
+      description: task,
+      completed: false
+    }
+
+    setTasks([...tasks, newTask])
+  }
 
   return (
     <>
       <Header /> 
 
       <main className={styles.container}>
-        <AddTask /> 
+        <AddTask onAddNewTask={addNewTask} /> 
 
-        <ListTasks />
+        <ListTasks tasks={tasks} setTasks={setTasks} />
       </main>
     </>
   )
